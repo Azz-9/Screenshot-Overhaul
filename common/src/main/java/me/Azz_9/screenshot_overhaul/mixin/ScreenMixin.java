e2edf8d53c82e75257d1dc56ada4af049f3985eb
Azz-9 <azz9lefaux@gmail.com>
2026-07-13 11:44:31 +0200

    fix preview widget bug quand on cliquait dessus c'était le dernier élément qui recevait l'évènement

--- common/src/main/java/me/Azz_9/screenshot_overhaul/mixin/ScreenMixin.java
+++ common/src/main/java/me/Azz_9/screenshot_overhaul/mixin/ScreenMixin.java
@@ -2,13 +2,21 @@ package me.Azz_9.screenshot_overhaul.mixin;
 
 import static me.Azz_9.screenshot_overhaul.CommonClass.MINECRAFT;
 
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.Screen;
 
+import org.jspecify.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 import me.Azz_9.screenshot_overhaul.client.gui.components.screenshotGallery.ScreenshotPreviewWidget;
 import me.Azz_9.screenshot_overhaul.client.preview.ScreenshotPreview;
@@ -16,18 +24,25 @@ import me.Azz_9.screenshot_overhaul.client.preview.ScreenshotPreview;
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
 
-	// Add screenshot preview on init if 
+	@Shadow
+	@Final
+	private List<GuiEventListener> children;
+	@Shadow
+	@Final
+	private List<NarratableEntry> narratables;
+	@Unique
+	private @Nullable ScreenshotPreviewWidget screenshot_overhaul$previewWidget;
+
+	// Add screenshot preview on init if
 	@Inject(method = "init(II)V", at = @At("TAIL"))
 	private void onInit(CallbackInfo ci) {
-		Screen self = (Screen) (Object) this;
-
 		// Add preview if it's already visible
 		if (ScreenshotPreview.isVisible()) {
-			screenshot_overhaul$addPreviewWidget(self);
+			screenshot_overhaul$addPreviewWidget();
 		}
 
 		// set a listener to add the preview when a screenshot is taken
-		ScreenshotPreview.setOnScreenshotSet(() -> MINECRAFT.execute(() -> screenshot_overhaul$addPreviewWidget(self)));
+		ScreenshotPreview.setOnScreenshotSet(() -> MINECRAFT.execute(this::screenshot_overhaul$addPreviewWidget));
 	}
 
 	@Inject(method = "onClose", at = @At("HEAD"))
@@ -39,10 +54,20 @@ public abstract class ScreenMixin {
 		ScreenshotPreview.setHovered(false);
 	}
 
+	@Inject(method = "extractRenderStateWithTooltipAndSubtitles", at = @At("TAIL"))
+	private void extractPreviewRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a, CallbackInfo ci) {
+		if (screenshot_overhaul$previewWidget != null) {
+			screenshot_overhaul$previewWidget.extractRenderState(graphics, mouseX, mouseY, a);
+		}
+	}
+
 	@Unique
-	private static void screenshot_overhaul$addPreviewWidget(Screen screen) {
+	private void screenshot_overhaul$addPreviewWidget() {
 		int screenW = MINECRAFT.getWindow().getGuiScaledWidth();
 		int screenH = MINECRAFT.getWindow().getGuiScaledHeight();
-		((ScreenAccessor) screen).invokeAddRenderableWidget(new ScreenshotPreviewWidget(screenW, screenH));
+		this.screenshot_overhaul$previewWidget = new ScreenshotPreviewWidget(screenW, screenH);
+		// the preview is added at the start of the list so the click events are sent to this widget first
+		this.children.addFirst(this.screenshot_overhaul$previewWidget);
+		this.narratables.addFirst(this.screenshot_overhaul$previewWidget);
 	}
 }
