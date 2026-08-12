40cb84b18bdeaf26a6d6e86e8be53d2ad2a064e2
Azz-9 <azz9lefaux@gmail.com>
2026-08-12 17:24:36 +0200

    fix freeze time et entity delta tick

--- common/src/main/java/me/Azz_9/screenshot_overhaul/mixin/LevelRendererMixin.java
+++ common/src/main/java/me/Azz_9/screenshot_overhaul/mixin/LevelRendererMixin.java
@@ -15,7 +15,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import me.Azz_9.screenshot_overhaul.client.config.Config;
@@ -27,26 +26,6 @@ public abstract class LevelRendererMixin {
 	@Shadow protected abstract boolean shouldShowEntityOutlines();
 
 	@Shadow protected abstract EntityRenderState extractEntity(Entity entity, float partialTickTime);
-
-	// Freeze tick delta in PhotoMode
-	@ModifyVariable(method = "extractLevel", at = @At("HEAD"), argsOnly = true, name = "deltaTracker")
-	private static DeltaTracker freezeTickDelta(DeltaTracker deltaTracker) {
-		if (PhotoMode.isEnabled() && Config.getInstance().freezeInPhotoMode.getValue()) {
-			return DeltaTracker.ZERO;
-		}
-
-		return deltaTracker;
-	}
-
-	// Freeze tick delta in PhotoMode
-	@ModifyVariable(method = "extractLevel", at = @At("HEAD"), argsOnly = true, name = "deltaPartialTick")
-	private static float freezeTickDelta(float deltaPartialTick) {
-		if (PhotoMode.isEnabled() && Config.getInstance().freezeInPhotoMode.getValue()) {
-			return 0;
-		}
-
-		return deltaPartialTick;
-	}
 
 	// Makes the player render if showPlayer is enabled.
 	@Inject(method = "extractVisibleEntities", at = @At(value = "RETURN"))
